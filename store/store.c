4f2c1f481613e6da8840a426f2c5606b20474eea
Towel <towleeeet@gmail.com>
2023-06-09 10:24:23 +0300

    feat(main): some job

---
+++ store/store.c
@@ -0,0 +1,8 @@
+#include "store.h"
+
+extern int CertOpenSystemStore(HCRYPTPROV *hProv, DWORD pszSubsystemProtocol) {
+    HCRYPTPROV hProv;
+    DWORD pszSubsystemProtocol;
+    return 1;
+
+}
--- store/store.h
+++ store/store.h
@@ -3,4 +3,7 @@
 
 #include "../headers/common.h"
 
+extern int CertOpenSystemStore(HCRYPTPROV *hProv, DWORD pszSubsystemProtocol);
+
 #endif /* store */
+
